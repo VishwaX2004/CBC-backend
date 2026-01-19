ddddb392a6cb2a1f5a986c7e64166330c780c8f1
Vishwa_Pramuditha <vishwapramuditha200@gmail.com>
2026-01-19 15:56:10 +0530

    Bug Fix 💕👍

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -2,13 +2,11 @@ import express from "express";
 import { sendContactMessage } from "../controllers/contactcontroller.js";
 
 
-const router = express.Router();
+const Contactrouter = express.Router();
 
 // POST /api/contact
-router.post("/", sendContactMessage);
+Contactrouter.post("/", sendContactMessage);
 
-<<<<<<< HEAD
-export default router;
-=======
-export default router;
->>>>>>> b82317c2b5cb2acfab7fe6cb1b1b6437d3a02eda
+export default Contactrouter
+
+
